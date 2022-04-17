09241c6401bf143fa040f38c621bd66ce3400b46
OMER FARUK KUCUKONDER <73589113+kajuboii@users.noreply.github.com>
2022-04-17 23:29:46 +0300

    added 1.5 days work file

---
+++ Days/1/1-2.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+        byte = 1 byte, -128 to 127
+        short = 2 byte -32,768 to 32,766
+        int = 4 byte whole numbers
+        long = 8 byte, whole numbers greater than int
+        float = 4 byte, 6-7 decimal digits
+        double = 8byte 15 decimal digits
+        boolean = 1 bit, true or false
+        char = 2 byte, single character
+        string = names, vb char list
+        */
+}}
---
+++ Days/2/2-1.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int sayi = 10;
+
+        if(sayi<20){
+            System.out.println("Number is less than 20.");
+        }
+        else if
+            System.out.println("Number is euqal to 20");
+        else
+            System.out.println("Number is greater than 20");
+
+}}
